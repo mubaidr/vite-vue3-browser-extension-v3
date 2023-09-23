0d994558e14c3244e05ad9252e21e7aa0101e5ee
Muhammad Ubaid Raza <mubaidr@gmail.com>
2023-09-23 15:40:31 +0500

    implement https://github.com/mubaidr/vite-vue3-chrome-extension-v3/issues/39

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,14 @@
+chrome.runtime.onInstalled.addListener(async (opt) => {
+  if (opt.reason === 'install') {
+    await chrome.storage.local.clear()
+
+    chrome.tabs.create({
+      active: true,
+      url: chrome.runtime.getURL('./installed.html'),
+    })
+  }
+})
+
 console.log('hello world from background')
 
 export {}
