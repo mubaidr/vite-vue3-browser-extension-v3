306d0ba0fbfe511bde1181cb8c5859d08c4bdc4c
Muhammad Ubaid Raza <mubaidr@gmail.com>
2023-10-03 14:52:37 +0500

    https://github.com/mubaidr/vite-vue3-chrome-extension-v3/issues/40

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('daisyui'),
+  ],
 }
