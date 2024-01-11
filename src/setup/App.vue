d3223563ab5e61df6b8ba2504ff6b101f4812cbb
Alexander <alexxycarlsson@gmail.com>
2024-01-11 02:28:35 +0100

    Forgot to uncomment

--- src/setup/App.vue
+++ src/setup/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-// import { inject, watchEffect } from 'vue'
+import { inject, watchEffect } from 'vue'
 
-// import { ISetup } from './index'
+import { ISetup } from './index'
 
-// const setup = inject<ISetup>('setupType')
+const setup = inject<ISetup>('setupType')
 
-// watchEffect(() => {
-//   if (setup) {
-//     if (setup.setupType === 'install') {
-//       document.title = `${__DISPLAY_NAME__} | Installed!`
-//     } else if (setup.setupType === 'update') {
-//       document.title = `${__DISPLAY_NAME__} | Updated!`
-//     }
-//   } else {
-//     document.title = __DISPLAY_NAME__
-//   }
-// })
+watchEffect(() => {
+  if (setup) {
+    if (setup.setupType === 'install') {
+      document.title = `${__DISPLAY_NAME__} | Installed!`
+    } else if (setup.setupType === 'update') {
+      document.title = `${__DISPLAY_NAME__} | Updated!`
+    }
+  } else {
+    document.title = __DISPLAY_NAME__
+  }
+})
 </script>
 
 <template>
