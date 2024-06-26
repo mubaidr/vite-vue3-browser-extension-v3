35cca52eadf610ef15d9bbc9b4efb9105a92d800
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-06-26 23:57:17 +0500

    Update vite-env.d.ts

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -4,6 +4,7 @@
 // Put your variables here:
 
 declare const __VERSION__: string
+declare const __NAME__: string
 declare const __DISPLAY_NAME__: string
 declare const __CHANGELOG__: string
 declare const __GIT_COMMIT__: string
