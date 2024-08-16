bddfe7661d72ee7a701ca466f17661bbb9de5dc9
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-08-16 12:15:44 +0500

    Update eslint.config.mjs and index.vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,6 @@ export default antfu(
     stylistic: false,
     typescript: true,
     vue: true,
-    comments: false,
   },
   {
     languageOptions: {
@@ -33,13 +32,15 @@ export default antfu(
   },
   {
     rules: {
+      'no-alert': 'warn',
       'no-console': 'warn',
       'no-restricted-globals': 'warn',
       'import/order': 'warn',
       '@typescript-eslint/ban-ts-comment': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
       'vue/multi-word-component-names': 'warn',
       '@eslint-community/eslint-comments/no-unlimited-disable': 'off',
+      'ts/explicit-function-return-type': 'off',
     },
   }
 )
--- src/content-script/iframe/pages/index.vue
+++ src/content-script/iframe/pages/index.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function handleClick() {
+  console.log(chrome.runtime)
+  alert('chrome.runtime is available in content script')
+}
+</script>
 
 <template>
   <div>
     <h1 class="text-red-800 font-bold text-xl">
       Content Script Iframe
     </h1>
-    <button class="btn btn-primary">
+    <button
+      class="btn btn-primary"
+      @click="handleClick"
+    >
       Sample Daisy UI Button
     </button>
   </div>
