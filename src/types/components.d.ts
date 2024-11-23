c256fb041178f0f19e16e4c35f8840d49b284604
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-11-23 23:36:54 +0500

    update deps, fix saas deprecation warn

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -8,6 +8,9 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Changelog: typeof import('./../components/Changelog.vue')['default']
+    IFaSolidDiceFive: typeof import('~icons/fa-solid/dice-five')['default']
+    'IHeroiconsOutline:menuAlt2': typeof import('~icons/heroicons-outline/menu-alt2')['default']
+    IHeroiconsOutlineMenuAlt2: typeof import('~icons/heroicons-outline/menu-alt2')['default']
     Install: typeof import('./../components/install.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
